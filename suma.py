b7d8767358aac421776b07d771621fdc16ac3d22
MyroslavaF <myroslava.farion@gmail.com>
2024-04-17 11:19:17 +0200

    Funciones implementados

--- suma.py
+++ suma.py
@@ -1,5 +1,20 @@
 def sumar(a, b):
-    return a + b;
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b == 0:
+        raise ZeroDivisionError("No se puede dividir entre 0")
+    return a / b
+    
 
 if __name__ == "__main__":
-    print(sumar(2,5));
+    print(sumar(3, 5));
+    print(restar(5, 3));
+    print(multiplicar(3, 5));
+    print(dividir(-6, 0));
--- test_suma.py
+++ test_suma.py
@@ -1,6 +1,9 @@
 import unittest
 
 from suma import sumar
+from suma import restar
+from suma import multiplicar
+from suma import dividir
 
 class TestSumar(unittest.TestCase):
 
@@ -8,6 +11,25 @@ class TestSumar(unittest.TestCase):
         self.assertEqual(sumar(3,2), 5)
         self.assertEqual(sumar(-1,1), 0)
         self.assertEqual(sumar(-1,-1), -2)
+
+    def test_restar(self):
+        self.assertEqual(restar(3, 2), 1)
+        self.assertEqual(restar(-3, 1), -4)
+        self.assertEqual(restar(-3,-1), -2)
+
+    def test_multiplicar(self):
+        self.assertEqual(multiplicar(3, 2), 6)
+        self.assertEqual(multiplicar(-2, 2), -4)
+        self.assertEqual(multiplicar(-2,-2), 4)
+
+
+    def test_dividir(self):
+        self.assertEqual(dividir(6, 2), 3)
+        self.assertEqual(dividir(-6, 2), -3)
+        self.assertEqual(dividir(-6,-2), 3)
+
+        with self.assertRaises(ZeroDivisionError):
+            dividir(6, 0)
 
 if __name__ == "__main__":
     unittest.main()
